3bee911baa8824a5a5fecf16822bcf5e29ee7398
bfraz <mrbrycefrazier@gmail.com>
2016-09-02 14:59:44 -0400

    2_4: add labels to color barchart

--- 2_4/color_barchart.js
+++ 2_4/color_barchart.js
@@ -1,7 +1,7 @@
 var w = 300;
-var h = 100;
+var h = 120;
 var padding = 2;
-var dataset = [5, 10, 15, 20,25];
+var dataset = [5, 10, 15, 21,25, 11, 25, 22, 18, 7];
 var svg = d3.select("body").append("svg")
            .attr("width", w).attr("height",h);
 
@@ -22,3 +22,17 @@ svg.selectAll("rect")
   width: w / dataset.length - padding,
   height: function(d){ return d*4;},
   fill: function(d){ return colorPicker(d);}}); //"rgb(" + (d*10) + ", 0,0)";
+
+svg.selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function(d){return d; })
+ .attr({
+  "text-anchor": "middle",
+  x: function(d, i){return i * (w / dataset.length) + (w / dataset.length - padding) / 2;},
+  y: function(d){return h - (d*4)+14; },
+  "font-family": "sans-serif",
+  "font-size": 12,
+  "fill": "#ffffff"
+});
