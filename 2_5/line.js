1327d633db3bb61554ad80c4816e994deb2c76fd
bfraz <mrbrycefrazier@gmail.com>
2016-09-02 16:00:53 -0400

    2_5: add labels to line

--- 2_5/line.js
+++ 2_5/line.js
@@ -1,24 +1,23 @@
-var h = 100;
-var w = 200;
+var h = 350;
+var w = 400;
 
 monthlySales = [
-  {"month": 10, "sales":20},
-  {"month": 20, "sales":14},
-  {"month": 30, "sales":20},
-  {"month": 40, "sales":21},
-  {"month": 50, "sales":15},
-  {"month": 60, "sales":22},
-  {"month": 70, "sales":9},
-  {"month": 80, "sales":6},
-  {"month": 90, "sales":23},
-  {"month": 100, "sales":7},
-
+  {"month": 10, "sales":100},
+  {"month": 20, "sales":130},
+  {"month": 30, "sales":250},
+  {"month": 40, "sales":300},
+  {"month": 50, "sales":265},
+  {"month": 60, "sales":225},
+  {"month": 70, "sales":180},
+  {"month": 80, "sales":120},
+  {"month": 90, "sales":145},
+  {"month": 100, "sales":130},
 ];
 
 var lineFun = d3.svg.line()
-    .x(function(d){ return d.month*2; })
-    .y(function(d){ return d.sales; })
-    .interpolate("linear");
+    .x(function(d){ return d.month*3; })
+    .y(function(d){ return h - d.sales; })
+    .interpolate("linear"); // basis to smooth out line
 
 var svg =
     d3.select("body").append("svg").attr({
@@ -31,3 +30,25 @@ var viz = svg.append("path")
   "stroke-width": 2,
   "fill": "none"
 });
+
+//add labels
+var labels = svg.selectAll("text")
+  .data(monthlySales)
+  .enter()
+  .append("text")
+ .text(function(d){ return d.sales; })
+ .attr({
+   x: function(d){ return d.month*3 - 25; },
+   y: function(d){ return h-d.sales;},
+   "font-size": "12px",
+   "font-family": "sans-serif",
+   "fill": "#666666",
+   "last-anchor": "start",
+   "dy": ".35em",
+   "font-weight": function(d,i) {
+     if ( i===0 || i==(monthlySales.length-1)){
+       return "bold";
+     }
+     return "normal";
+   }
+ });
